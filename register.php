6918c4e81ad4019b667ba9aeb3ea782e9308e7eb
Ahmad Syaifuddin <ahmadsyai598@gmail.com>
2023-12-24 10:36:29 +0800

    Update register.php

--- register.php
+++ register.php
@@ -59,11 +59,12 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css" />
+    <link rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css" />
     <link rel="stylesheet" href="style1.css">
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.10.1/dist/sweetalert2.all.min.js"></script>
+    <link href="https://cdn.jsdelivr.net/npm/sweetalert2@11.10.1/dist/sweetalert2.min.css" rel="stylesheet">
     <title>Register</title>
-
-
 
 </head>
 
@@ -78,7 +79,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
             <div class="input-group" style="margin-bottom: 10px">
                 <label for="email">Email </label>
-                <input type="email" id="email" name="email" class="form-control" placeholder="unknown@gmail.com" required><br>
+                <input type="email" id="email" name="email" class="form-control" placeholder="unknown@gmail.com"
+                    required><br>
             </div>
 
             <div class="input-group" style="margin-bottom: 10px;">
@@ -97,6 +99,9 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             <div class="input-group" style="margin-bottom: 10px;">
                 <label for="confirm_password">Konfirmasi Password </label>
                 <input type="password" id="confirm_password" class="form-control" name="confirm_password" required><br>
+                <button type="button" id="btnToggle2" class="toggle2">
+                    <i id="eyeIcon" class="fa fa-eye"></i>
+                </button>
             </div>
 
             <div class="input-group" style="margin-bottom: 10px;">
@@ -109,7 +114,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
             <div class="input-group" style="margin-bottom: 10px;">
                 <label for="ttl">Tempat dan Tanggal Lahir </label>
-                <input type="text" id="ttl" class="form-control" placeholder="Denpasar, 09-Juni-2004" name="ttl" required>
+                <input type="text" id="ttl" class="form-control" placeholder="Denpasar, 09-Juni-2004" name="ttl"
+                    required>
             </div>
 
             <div class="input-group" style="margin-bottom: 10px;">
@@ -118,7 +124,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             </div>
 
 
-            <button type="submit" class="btn btn-lg btn-primary btn-block" id="submit_form" name="submit" value=<?php echo date('l/d/M/Y-H:i:s:a'); ?>>R E G I S T E R</button><br>
+            <button type="submit" class="btn btn-lg btn-primary btn-block" id="submit_form" name="submit"
+                value=<?php echo date('l/d/M/Y-H:i:s:a'); ?>>R E G I S T E R</button><br>
             <p> <b>Sudah mempunyai akun ?</b> <a href="login.php">Login</a></p>
         </form>
 
@@ -129,67 +136,68 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
 
     <script>
-        $(document).ready(function() {
-            const passwordInput = document.getElementById('password');
-            const toggleButton = document.getElementById('btnToggle');
-            const eyeIcon = document.getElementById('eyeIcon');
+    $(document).ready(function() {
+        const passwordInput = document.getElementById('password');
+        const toggleButton = document.getElementById('btnToggle');
+        const toggleButton2 = document.getElementById('btnToggle2');
+        const eyeIcon = document.getElementById('eyeIcon');
 
-            toggleButton.addEventListener('click', function() {
-                if (passwordInput.type === 'password') {
-                    passwordInput.type = 'text';
-                    eyeIcon.classList.remove('fa-eye');
-                    eyeIcon.classList.add('fa-eye-slash');
-                } else {
-                    passwordInput.type = 'password';
-                    eyeIcon.classList.remove('fa-eye-slash');
-                    eyeIcon.classList.add('fa-eye');
-                }
-            });
-
-            /**
-            $("#submit_form").click(function() {
-                var passwordregex = /^(.{0,7}|[^0-9]*|[^A-Z]*|[a-zA-Z0-9]*)$/;
-                var emailRegex = new RegExp(/^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i);
-                var username = $.trim($("#username").val());
-                var email = $.trim($("#email").val());
-                var Password = $.trim($("#password").val());
-                $("#username_error").html("");
-                $("#password_error").html("");
-                $("#email_error").html("");
-                if (username == "") {
-                    $("#username").focus();
-                    $("#username_error").show();
-                    $("#username_error").html("Please Enter Username");
-                    return false;
-                } else if (email == "") {
-                    $("#email").focus();
-                    $("#email_error").show();
-                    $("#email_error").html("Please Enter Email");
-                    return false;
-                } else if (!email.match(emailRegex)) {
-                    $("#email").focus();
-                    $("#email_error").show();
-                    $("#email_error").html("Enter Valid Email");
-                    return false;
-                } else if (Password == "") {
-                    $("#password").focus();
-                    $("#password_error").show();
-                    $("#password_error").html("Please Enter Password");
-                    return false;
-                } else if (Password.match(passwordregex)) {
-                    $("#password").focus();
-                    $("#password_error").show();
-                    $("#password_error").html(
-                        "Make a strong password, 8 characters, including an uppercase letter, number, and one special character."
-                    );
-                    return false;
-                } else {
-                    alert("wefw");
-                }
-            });
-             */
-
+        toggleButton.addEventListener('click', function() {
+            if (passwordInput.type === 'password') {
+                passwordInput.type = 'text';
+                eyeIcon.classList.remove('fa-eye');
+                eyeIcon.classList.add('fa-eye-slash');
+            } else {
+                passwordInput.type = 'password';
+                eyeIcon.classList.remove('fa-eye-slash');
+                eyeIcon.classList.add('fa-eye');
+            }
         });
+
+        /**
+        $("#submit_form").click(function() {
+            var passwordregex = /^(.{0,7}|[^0-9]*|[^A-Z]*|[a-zA-Z0-9]*)$/;
+            var emailRegex = new RegExp(/^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i);
+            var username = $.trim($("#username").val());
+            var email = $.trim($("#email").val());
+            var Password = $.trim($("#password").val());
+            $("#username_error").html("");
+            $("#password_error").html("");
+            $("#email_error").html("");
+            if (username == "") {
+                $("#username").focus();
+                $("#username_error").show();
+                $("#username_error").html("Please Enter Username");
+                return false;
+            } else if (email == "") {
+                $("#email").focus();
+                $("#email_error").show();
+                $("#email_error").html("Please Enter Email");
+                return false;
+            } else if (!email.match(emailRegex)) {
+                $("#email").focus();
+                $("#email_error").show();
+                $("#email_error").html("Enter Valid Email");
+                return false;
+            } else if (Password == "") {
+                $("#password").focus();
+                $("#password_error").show();
+                $("#password_error").html("Please Enter Password");
+                return false;
+            } else if (Password.match(passwordregex)) {
+                $("#password").focus();
+                $("#password_error").show();
+                $("#password_error").html(
+                    "Make a strong password, 8 characters, including an uppercase letter, number, and one special character."
+                );
+                return false;
+            } else {
+                alert("wefw");
+            }
+        });
+         */
+
+    });
     </script>
 
 
